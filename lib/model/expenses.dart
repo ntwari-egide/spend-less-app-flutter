5507bad01d63a8f8b6d097d98791d22e1751f567
ntwari-egide <ntwariegide2@gmail.com>
2023-10-22 14:13:58 -0400

    feat: add enum

--- lib/model/expenses.dart
+++ lib/model/expenses.dart
@@ -3,15 +3,28 @@ import 'package:uuid/uuid.dart';
 
 const uuid = Uuid();
 
+// adding expense type enum
+enum ExpenseType {
+  food,
+  travel,
+  bills,
+  transport,
+  entertainment,
+  leisure,
+  work,
+}
+
 class Expense {
   Expense({
     required this.title,
     required this.amount,
     required this.date,
+    required this.expenseType,
   }) : id = uuid.v4();
 
   final String id;
   final String title;
   final double amount;
   final DateTime date;
+  final ExpenseType expenseType;
 }
