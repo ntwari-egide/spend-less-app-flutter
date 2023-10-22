85ffa06f7c96da65916a4fb78168c2eb4aae8b67
ntwari-egide <ntwariegide2@gmail.com>
2023-10-22 14:25:57 -0400

    feat: implement exenses list

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,9 +1,9 @@
 // class widget
 
 import 'package:flutter/material.dart';
+import 'package:spend_less/model/expenses.dart';
 
 class ExpensesWidget extends StatefulWidget {
-
   const ExpensesWidget({super.key});
 
   @override
@@ -13,8 +13,26 @@ class ExpensesWidget extends StatefulWidget {
 }
 
 class _ExpensesWidgetState extends State<ExpensesWidget> {
+  // defining dummy data
+  final List<Expense> expenses = [
+    Expense(
+        title: 'Chicken',
+        amount: 32,
+        date: DateTime.now(),
+        expenseType: ExpenseType.food),
+    Expense(
+        title: 'Pepsi',
+        amount: 23,
+        date: DateTime.now(),
+        expenseType: ExpenseType.transport),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp( home:  Scaffold(body: Text("new app"),),);
+    return const MaterialApp(
+      home: Scaffold(
+        body: Text("new app"),
+      ),
+    );
   }
 }
---
+++ lib/view/expenses_list.dart
@@ -0,0 +1,15 @@
+// expenses list class
+
+import 'package:flutter/material.dart';
+import 'package:spend_less/model/expenses.dart';
+
+class ExpensesList extends StatelessWidget {
+  const ExpensesList({super.key, required this.allExpenses});
+
+  final List<Expense> allExpenses;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(itemCount: allExpenses.length ,itemBuilder: (ctx, index) => Text(allExpenses[index].title), );
+  }
+}
